0170497714f731e13d81af974660e2181b78f2e8
isamertfiliz <31957384+isamertfiliz@users.noreply.github.com>
2018-02-18 21:43:00 +0300

    .NetCoreWebApi

---
+++ KullaniciWepApi/KullaniciWepApi/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KullaniciWepApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KullaniciWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.User.Count()==0)
+            {
+                _context.User.Add(new User { Name = "İsa" });
+                _context.SaveChanges();
+
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetAll()
+        {
+            return _context.User.ToList() ;
+        }
+
+        [HttpGet("{id}", Name = "GetUser")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.User.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _context.User.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetUser", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(long id, [FromBody] User item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var user = _context.User.FirstOrDefault(t => t.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = item.Name;
+            user.Surname = item.Surname;
+            user.Status = item.Status;
+            
+
+            _context.User.Update(user);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(long id)
+        {
+            var user = _context.User.FirstOrDefault(t => t.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
---
+++ KullaniciWepApi/KullaniciWepApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KullaniciWepApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Status { get; set; }
+    }
+}
